4e99b059d6505d0aa6f5efe3f6b626bee8b1b647
smrutikapatil <smruti.patil17@gmail.com>
2022-03-10 20:35:00 +0530

    Ability ro analyze And respond happy or sad mood

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,6 +1,17 @@
 public class MoodAnalyzer {
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else if (message.toLowerCase().contains("happy")) {
+            return "Happy";
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to mood analyser programs");
+        String mood = MoodAnalyzer.analyseMood("User is Happy");
+        System.out.println(mood);
+        mood = MoodAnalyzer.analyseMood("User is Sad");
+        System.out.println(mood);
     }
 }
