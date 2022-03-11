de9bade169c22ceb14d866d1e5c4eae66b49826a
smrutikapatil <smruti.patil17@gmail.com>
2022-03-11 15:13:09 +0530

    Added- Refractore code To take the Message Should Return Sad in Test Case

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,8 +1,28 @@
 public class MoodAnalyzer {
-    public static String analyseMood(String message) {
+    public static String message;
+
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+        analyseMood();
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public static String analyseMood() {
+
         if (message.toLowerCase().contains("sad")) {
             return "Sad";
-        } else if (message.toLowerCase().contains("Any")) {
+        } else if (message.toLowerCase().contains("happy")) {
         }
         return "Happy";
     }
