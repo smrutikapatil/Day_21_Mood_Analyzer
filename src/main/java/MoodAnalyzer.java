7c649414f1bdcbf066cb2da196323e1a2dd9a4c5
smrutikapatil <smruti.patil17@gmail.com>
2022-03-11 15:06:21 +0530

    added- Code to analyse Mood message Should Return happy in Test Case

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,7 +1,9 @@
 public class MoodAnalyzer {
     public static String analyseMood(String message) {
         if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else if (message.toLowerCase().contains("Any")) {
         }
-        return "Sad";
+        return "Happy";
     }
 }
